4c34e41e2015729e550e2ab150d9327ed23791c5
Terry Johnson <terence.johnson@gmail.com>
2024-09-24 17:42:51 +0100

    Add files via upload
    Redirect to Alternative Python code.

---
+++ Link_to_Python_Version.py
@@ -0,0 +1,14 @@
+# A Python version of the C++ program (approved by myself) can be found here.
+#
+# https://github.com/minh-nguyenhoang/Enhanced-Image-Colour-Transfer-2
+# 
+# Look for 'enhance 2.py'
+
+import webbrowser
+
+url = "https://github.com/minh-nguyenhoang/Enhanced-Image-Colour-Transfer-2"
+webbrowser.open(url)
+
+url = "https://github.com/minh-nguyenhoang/Enhanced-Image-Colour-Transfer-2/blob/main/enhance2.py"
+webbrowser.open(url)
+
